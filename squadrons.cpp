595a49fd6641284b7baf2cc5c915f2e1f5a6e02a
Fred Emmott <fred@fredemmott.com>
2020-12-20 16:06:33 -0600

    Initial commit, with profile for Star Wars: Squadrons

---
+++ squadrons.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020-present, Fred Emmott <fred@fredemmott.com>
+ * All rights reserved.
+ *
+ * This source code is licensed under the ISC license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+#include "easymode.h"
+
+int main() {
+  auto [p, throttle, stick, vj1, vj2] = create_profile(
+    TM_WARTHOG_THROTTLE,
+    VPC_RIGHT_WARBRD,
+    VJOY_1,
+    VJOY_2
+  );
+
+  p->passthrough(throttle, vj1);
+  p->passthrough(stick, vj2);
+
+  // Replace the ministick mappings on both devices to additional buttons
+  p->map({
+    {
+      throttle.XAxis,
+      AxisToButtons {
+        { 0, 5, vj1.button(throttle.ButtonCount + 1) },
+        { 95, 100, vj1.button(throttle.ButtonCount  + 2) }
+      },
+    }, {
+      throttle.YAxis,
+      AxisToButtons {
+        { 0, 5, vj1.button(throttle.ButtonCount + 3) },
+        { 95, 100, vj1.button(throttle.ButtonCount + 4) }
+      },
+    }, {
+      stick.RXAxis,
+      AxisToButtons {
+        { 0, 5, vj2.button(stick.ButtonCount + 1) },
+        { 95, 100, vj2.button(stick.ButtonCount + 2) }
+      },
+    }, {
+      stick.RYAxis,
+      AxisToButtons {
+        { 0, 5, vj2.button(stick.ButtonCount + 3) },
+        { 95, 100, vj2.button(stick.ButtonCount + 4) }
+      },
+    },
+  });
+
+  p->run();
+  return 0;
+}
