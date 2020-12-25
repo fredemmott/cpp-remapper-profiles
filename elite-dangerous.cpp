4671a81292f00339a387e4c36bc7e876fecb8c04
Fred Emmott <fred@fredemmott.com>
2020-12-25 15:18:26 -0600

    Add massive deadzones for thumbsticks

--- elite-dangerous.cpp
+++ elite-dangerous.cpp
@@ -9,10 +9,12 @@
 
 /** Mapping for Elite Dangerous.
  *
- * - add tiny deadzones to all axis: if you have sensitive devices, the moment
+ * - add tiny deadzones to XY axis: if you have sensitive devices, the moment
  *   docking clamps are released, you start drifting. In game deadzones are per
  *   axis and just sliders with no numbers, so really annoying to get
  *   consistent.
+ * - add big deadzones to ministicks for menu navigation. Originally did
+ *   buttons, but Elite is restricted to 32 buttons
  */
 int main() {
   auto [p, l, r, vl, vr] = create_profile(
@@ -26,10 +28,16 @@ int main() {
   p->passthrough(r, vr);
 
   p->map({
-    { l.XAxis, SquareDeadzone { 10, vl.XAxis } },
-    { l.YAxis, SquareDeadzone { 10, vl.YAxis } },
-    { r.XAxis, SquareDeadzone { 10, vr.XAxis } },
-    { r.YAxis, SquareDeadzone { 10, vr.YAxis } },
+    // XY deadzones
+    { l.XAxis, SquareDeadzone { 5, vl.XAxis } },
+    { l.YAxis, SquareDeadzone { 5, vl.YAxis } },
+    { r.XAxis, SquareDeadzone { 5, vr.RXAxis } },
+    { r.YAxis, SquareDeadzone { 5, vr.RYAxis } },
+    // Thumbstick deadzones
+    { l.RXAxis, SquareDeadzone { 90, vl.RXAxis } },
+    { l.RYAxis, SquareDeadzone { 90, vl.RYAxis } },
+    { r.RXAxis, SquareDeadzone { 90, vr.RXAxis } },
+    { r.RYAxis, SquareDeadzone { 90, vr.RYAxis } },
   });
 
   p->run();
