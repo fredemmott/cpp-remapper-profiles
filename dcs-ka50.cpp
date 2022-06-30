098354eff0915a90697c82a94bdb1d03c11c7e35
Fred Emmott <git@fredemmott.com>
2022-06-30 18:39:05 -0500

    add vaicom and OpenKneeboard bindings to Ka50 profile

--- dcs-ka50.cpp
+++ dcs-ka50.cpp
@@ -6,14 +6,60 @@
  * in the root directory of this source tree.
  */
 
+#include "common_buttons.h"
 #include "easymode.h"
 
 int main() {
-  auto [p, cyclic, vj, _hide_unused_vj2]
-    = create_profile(VPC_MT50CM2_SHARKA_CYCLIC, VJOY_1, VJOY_2_IN);
+  auto [p, cyclic, collective, vj, _hide_unused_vj2] = create_profile(
+    VPC_MT50CM2_SHARKA_CYCLIC,
+    UnhiddenDevice {VPC_ROTOR_TCS_PLUS},
+    VJOY_1,
+    VJOY_2_IN);
 
   // Pass through everything :)
   cyclic >> vj;
+  auto& vcommon = vj;
+
+  bool shifted = false;
+  // Red button
+  collective.Button20 >> &shifted;
+
+  // Hat
+  cyclic.Hat1 >> Shift {
+    &shifted,
+    // DCS only offers N/E/S/W bindings for the shkval slew, but if you bind
+    // hat N to shkval slew N, it *will not* be active if hat is NE
+    //
+    // ... however, if you press both the 'north' and 'east' buttons, you get
+    // north-east movement - so, replace the hat with 4 buttons, with 9 states:
+    // center, n, n+e, e, e+s, s, s+w, w, w+n
+    HatToButtons(&vj, cyclic.getButtonCount() + 1, 4),
+    HatToButtons {
+      HatToButtons::Interpolation::None,
+      vcommon.button(ButtonIDs::OKB_PREVIOUS_TAB),
+      vcommon.button(ButtonIDs::OKB_NEXT_PAGE),
+      vcommon.button(ButtonIDs::OKB_NEXT_TAB),
+      vcommon.button(ButtonIDs::OKB_PREVIOUS_PAGE),
+    },
+  };
+  // Trigger
+  cyclic.Button1 >> Shift {
+    &shifted,
+    vj.Button1,
+    vcommon.button(ButtonIDs::RECENTER_VR_HEADSET),
+  };
+  // Top right (radio button)
+  cyclic.Button10 >> Shift {
+    &shifted,
+    vcommon.button(ButtonIDs::VAICOM_TX_1),
+    vj.Button10,
+  };
+  // Right side press (unused)
+  cyclic.Button17 >> Shift {
+    &shifted,
+    vj.Button17,
+    vcommon.button(ButtonIDs::OKB_SWITCH_KNEEBOARDS),
+  };
 
   // I like the firmware to preserve as much information as possible, to I have
   // the trigger guard button indicate the current state, instead of triggering
@@ -24,14 +70,6 @@ int main() {
   //
   // If the state is wrong in game, press 'c' on the keyboard to fix it.
   cyclic.Button28 >> LatchedToMomentaryButton() >> vj.Button28;
-
-  // DCS only offers N/E/S/W bindings for the shkval slew, but if you bind
-  // hat N to shkval slew N, it *will not* be active if hat is NE
-  //
-  // ... however, if you press both the 'north' and 'east' buttons, you get
-  // north-east movement - so, replace the hat with 4 buttons, with 9 states:
-  // center, n, n+e, e, e+s, s, s+w, w, w+n
-  cyclic.Hat1 >> HatToButtons(&vj, cyclic.getButtonCount() + 1, 4);
 
   AxisTrimmer xtrim, ytrim;
   cyclic.XAxis >> &xtrim >> vj.XAxis;
